7bba9f93f0974be390a3481073d94b5b568e71a4
git-chittaranjan <git.chittaranjan@gmail.com>
2025-07-21 16:19:12 +0530

    Revert "4th commit"
    This reverts commit c00d55e553cf712c953fef19bb7952e7ea2ac7ec.

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ function App() {
       <div>Initial Code</div>
       <div>2nd - Adding 2nd line</div>
       <div>3rd - Adding 3rd line</div>
-      <div>4th - Adding 4th line</div>
     </div>
   )
 }
