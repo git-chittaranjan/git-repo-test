6e57baa5bbc4d8b01a1702d5559f20149011d125
git-chittaranjan <git.chittaranjan@gmail.com>
2025-07-21 16:29:17 +0530

    Re-adding 5th line

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,13 @@
 function App() {
 
   return (
-<<<<<<< HEAD
-    <div>Initial Code</div>
-=======
     <div>
       <div>Initial Code</div>
       <div>2nd - Adding 2nd line</div>
       <div>3rd - Adding 3rd line</div>
       <div>4th - Adding 4th line</div>
+      <div>5th - Adding 5th line again</div>
     </div>
->>>>>>> parent of 149ecd3 (Reapply "5th commit")
   )
 }
 
