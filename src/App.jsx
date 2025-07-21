f080220433a5f0f3b9dbf590f549a80887ce0b44
git-chittaranjan <git.chittaranjan@gmail.com>
2025-07-21 16:26:43 +0530

    reverting the commit till 4th stage

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,16 @@
 function App() {
 
   return (
+<<<<<<< HEAD
     <div>Initial Code</div>
+=======
+    <div>
+      <div>Initial Code</div>
+      <div>2nd - Adding 2nd line</div>
+      <div>3rd - Adding 3rd line</div>
+      <div>4th - Adding 4th line</div>
+    </div>
+>>>>>>> parent of 149ecd3 (Reapply "5th commit")
   )
 }
 
